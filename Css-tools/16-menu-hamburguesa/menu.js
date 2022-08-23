de7ce1826665226eafeb76a5efaab4fd64059acc
Olimpico <95925621+Olimpico16@users.noreply.github.com>
2022-08-23 17:47:28 -0500

    Delete Css-tools directory

--- Css-tools/16-menu-hamburguesa/menu.js
+++
@@ -1,7 +0,0 @@
-const menus = document.querySelectorAll('.menu');
-menus.forEach((menu) =>{
-    const hambergerMenu = menu.querySelector('.hamberger-menu');
-    menu.addEventListener('click',()=>{
-        hambergerMenu.classList.toggle('open');
-    });
-});
